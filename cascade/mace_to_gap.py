58c0d54061f80435d5b6024109ed96199eb74f3d
Choiwah.Chow <choiwah.chow@gmail.com>
2022-11-01 19:08:40 -0400

    add mace_to_gap.py

---
+++ cascade/mace_to_gap.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import re
+
+
+def conv_a_row(line):
+    m = re.search(r"\d", line)
+    m2 = re.match('.+([0-9])[^0-9]*$', line)
+    a = line[m.start(): m2.start(1)+1]
+    b = a.split(',')
+    
+    return [int(x)+1 for x in b]
+
+
+def conv_a_model(fp, delim):
+    line = fp.readline()
+    arr = list()
+    while line:
+        row = conv_a_row(line)
+        arr.append(row)
+        if delim in line:
+            return arr
+        line = fp.readline()
+    return arr
+
+
+def conv_a_file(fn, delim):
+    all_models = list()
+    with (open(fn)) as fp:
+        line = fp.readline()
+        while line:
+            if "function" in line:
+                model = conv_a_model(fp, delim)
+                all_models.append(model)
+            line = fp.readline()
+    return all_models
+
+
+if __name__ == "__main__":
+    delim = "])])."
+    # print(conv_a_row("    [[1,2,3,4,5,6],"))
+    all_models = conv_a_file("models_200.out", delim)
+    for m in all_models:
+        print(m)
+
+    # print(all_models)
