ce4e1dd75b4b15fb0903b66e857a8e6a5f4fb13f
Choiwah Chow <choiwah@musca.inesc-id.pt>
2022-12-18 02:55:00 +0000

    for k=11

--- cascade/iso.sh
+++ cascade/iso.sh
@@ -3,6 +3,6 @@
 k=11
 
 mkdir -p working${k}
-../../splitModels/splitModels -d${k} -f"../../bin/isofilter -ignore_constants" -m100 -oworking${k}/m11_ -tworking${k}/statistics_${k}.json -u16000000 -s1000 -r50 -l20 -x3000 -i"monoid_11.out" > filter${k}.log 2>&1 
+../../bin/splitModels -d${k} -f"../../bin/isofilter -ignore_constants" -m100 -oworking${k}/m${k}_ -tworking${k}/statistics_${k}.json -u16000000 -s1000 -r50 -l20 -x3000 -i"monoid_${k}.out" > filter${k}.log 2>&1 
 
 
